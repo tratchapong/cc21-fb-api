1b72b7d7b7312ba632fa32cbed00dff6f8714697
Ratchapong <ratchapong@Ratchapongs-Mac-mini.local>
2025-10-29 22:47:31 +0700

    add user.service

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,7 @@ import createHttpError from "http-errors"
 import identityKeyUtil from "../utils/identity-key.util.js"
 import prisma from "../config/prisma.config.js"
 import { loginSchema, registerSchema } from '../validations/schema.js'
+import { createUser, getUserBy } from '../services/user.service.js'
 
 export const register = async (req, res, next) => {
   const { identity, firstName, lastName, password, confirmPassword } = req.body
@@ -11,13 +12,11 @@ export const register = async (req, res, next) => {
   const user = registerSchema.parse(req.body)
   const identityKey = user.email ? 'email' : 'mobile'
   // find user if already have registered
-  const haveUser = await prisma.user.findUnique({
-    where: { [identityKey]: identity }
-  })
+  const haveUser = await getUserBy(identityKey, identity)
   if (haveUser) {
     return next(createHttpError[409]('This user already register'))
   }
-  const result = await prisma.user.create({ data: user })
+  const result = await createUser(user)
   res.json({
     msg: 'Register Successful',
     result
@@ -27,9 +26,7 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   const { identity, password } = req.body
   const user = loginSchema.parse(req.body)
-  const foundUser = await prisma.user.findUnique({
-    where : { [user.email? 'email' : 'mobile'] : identity}
-  })
+  const foundUser = await getUserBy(user.email? 'email' : 'mobile', identity)
   // have no this user
   if(!foundUser) {
     return next(createHttpError[401]('Invalid Login'))
