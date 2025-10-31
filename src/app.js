507c401b9e96c6b28ab8d81eb26325efea671a0c
tratchapong <tratchapong@gmail.com>
2025-10-31 09:31:53 +0700

    add rate-limit, helmet, morgan

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
+import morgan from 'morgan'
 import authRoute from './routes/auth.route.js'
 import createHttpError from 'http-errors'
 import errorMiddleware from './middlewares/error.middleware.js'
@@ -7,11 +10,19 @@ import notFoundMiddleware from './middlewares/notFound.middleware.js'
 import shutdownUtil from './utils/shutdown.util.js'
 
 const app = express()
+app.use(morgan("dev"))
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max : 100,
+}))
+app.use(helmet())
+
 app.use(cors({
   origin: ["http://localhost:5173"], // allowed origins
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true, // allow cookies if needed
 }));
+
 app.use(express.json())
 
 app.use('/api/auth', authRoute)
