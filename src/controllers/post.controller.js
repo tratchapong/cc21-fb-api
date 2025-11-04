07539b223e2fbd637caf12c229f9e710f719ea65
tratchapong <tratchapong@gmail.com>
2025-11-04 14:44:13 +0700

    deletePost done

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import createHttpError from "http-errors"
 import prisma from "../config/prisma.config.js"
 
 
@@ -32,5 +33,24 @@ export const createPost = async (req, res) => {
 	res.status(201).json({
 		message : 'Create new Post done',
 		result
+	})
+}
+
+export const deletePost = async (req, res, next) => {
+	const {id} = req.params
+
+	const foundPost = await prisma.post.findUnique({
+		where : { id : +id}
+	})
+	if(!foundPost) {
+		return next (createHttpError[404]('Data not found'))
+	}
+	if(req.user.id != foundPost.userId) {
+		return next (createHttpError[401]('Cannot delete this post'))
+	}
+
+	const result = await prisma.post.delete({where : {id: +id}})
+	res.json({
+		message: 'Delete done'
 	})
 }
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createPost, getAllPosts } from "../controllers/post.controller.js";
+import { createPost, deletePost, getAllPosts } from "../controllers/post.controller.js";
 
 const postRoute = Router()
 
 postRoute.get('/', getAllPosts)
 postRoute.post('/', createPost)
+postRoute.delete('/:id', deletePost)
 
 
 export default postRoute
